af4e026a16d544ef2e6240fb4cc7efda307902b7
nikolaivlasov <60992281+nikolaivlasov@users.noreply.github.com>
2025-10-12 02:27:33 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Заявка")
+
+order_destination = st.text_area('Назначение',"")
+if order_destination:
+    st.badge("", icon=":material/check:", color="green")
+else:
+    st.markdown(
+        ":orange-badge[⚠️ необходимо заполнить] "
+    )
+
+order_types_names=["корпоративная карта","мне на ИП/СЗ","счет от организации"]
+order_type=st.radio("**Тип оплаты:**",order_types_names,index=None,help="в разработке",horizontal=True)
+if order_type == "корпоративная карта":
+    st.badge("пополнить карту", icon=":material/check:", color="green")
+else:
+    st.markdown(
+        ":orange-badge[⚠️ необходимо заполнить] "
+    )
