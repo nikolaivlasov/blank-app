c3712d3efbd0cd17eef11a2b3081e38c6e09828f
nikolaivlasov <60992281+nikolaivlasov@users.noreply.github.com>
2025-10-14 21:18:32 +0000

    11

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,14 @@
 import streamlit as st
 
-
-
-
-
 st.title("Заявка")
 adrt = st.query_params["first_key"]
 st.badge(adrt, color="blue")
-
 if st.query_params["first_key"] == "1":
     st.title("нашел")
+
 with st.container(border=True):
     order_types_names = ["корпоративная карта", "мне на ИП/СЗ", "счету или договору др. юр.лица"]
-    order_type = st.radio("**Тип оплаты:**", order_types_names, index=None,
+    order_type = st.radio("**1. Тип оплаты:**", order_types_names, index=None,
                           help="В одной заявке нельзя использовать разные типы оплат", horizontal=True)
     if order_type == "корпоративная карта":
         st.badge("пополнить карту", icon=":material/check:", color="green")
@@ -26,13 +22,13 @@ with st.container(border=True):
         )
 
 with st.container(border=True):
-
-    order_destination = st.text_area('**Назначение:**',help="можно несколько")
+    order_destination = st.text_area('**2. Назначение:**',help="можно несколько",placeholder="1. закупка костюма персонажа ММ с подбором на примерку 75000 р. \n2. хоз.инвентарь 18000 р.")
     if order_destination:
         st.badge("", icon=":material/check:", color="green")
     else:
         st.markdown(
             ":orange-badge[⚠️ необходимо заполнить] "
         )
+
 
 
