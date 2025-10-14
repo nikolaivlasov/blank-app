7c3422a11c887a956e95cd78f6058f514bb9f373
nikolaivlasov <60992281+nikolaivlasov@users.noreply.github.com>
2025-10-14 21:31:33 +0000

    13

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.logo("Логотип1.jpg",size ="large")
+st.logo("Логотип1.jpg",size ="large",link="https://lazydog.tv/")
 st.title("Заявка")
 adrt = st.query_params["first_key"]
 st.badge(adrt, color="blue")
