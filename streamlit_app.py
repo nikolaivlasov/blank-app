b1413c694a281be2525ff4d1d0ea163fa1a19239
nikolaivlasov <60992281+nikolaivlasov@users.noreply.github.com>
2025-10-14 19:54:15 +0000

    новый

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,26 @@
 import streamlit as st
 
+adrt=st.query_params["first_key"]
+st.title(adrt)
+
 
 if st.query_params["first_key"] == "1":
     st.title("нашел")
-st.title("Заявка")
 
-order_destination = st.text_area('Назначение',"")
+st.title("Заявка")
+order_types_names=["корпоративная карта","мне на ИП/СЗ","по счету или договору др. юр.лица"]
+order_type=st.radio("**Тип оплаты:**",order_types_names,index=None,help="нельзя в одной заявке использовать разные типы оплат",horizontal=True)
+if order_type == "корпоративная карта":
+    st.badge("пополнить карту", icon=":material/check:", color="green")
+if order_type == "мне на ИП/СЗ":
+    st.badge("дополнить приложение к вашему договору", icon=":material/check:", color="green")
+if order_type == "по счету или договору др. юр.лица":
+    st.badge("укажите название юр.лица в комментариях", icon=":material/check:", color="green")
+else:
+    st.markdown(
+        ":orange-badge[⚠️ необходимо заполнить] "
+    )
+order_destination = st.text_area('Назначение',"",help="можно несколько")
 if order_destination:
     st.badge("", icon=":material/check:", color="green")
 else:
@@ -13,11 +28,3 @@ else:
         ":orange-badge[⚠️ необходимо заполнить] "
     )
 
-order_types_names=["корпоративная карта","мне на ИП/СЗ","счет от организации"]
-order_type=st.radio("**Тип оплаты:**",order_types_names,index=None,help="в разработке",horizontal=True)
-if order_type == "корпоративная карта":
-    st.badge("пополнить карту", icon=":material/check:", color="green")
-else:
-    st.markdown(
-        ":orange-badge[⚠️ необходимо заполнить] "
-    )
