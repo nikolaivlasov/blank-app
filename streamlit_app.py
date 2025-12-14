d217f21fafaa484063659c8b3c5a05663c8d0eb5
nikolaivlasov <60992281+nikolaivlasov@users.noreply.github.com>
2025-12-14 14:55:14 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -132,7 +132,7 @@ elif st.session_state.page == 5:
         st.markdown('Приятного просмотра!')
 
     with st.container(border=True):
-        st.image("present_1.jpg", caption="Поход в кино", width=200)
+        st.image("present_1.jpg", caption="Поход в кино", width=300)
 
     with st.container(border=True):
         st.image(st.session_state["selected_image_path"], width=200, clamp=True)
